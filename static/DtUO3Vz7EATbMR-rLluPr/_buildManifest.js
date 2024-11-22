97fc863673e61f8bda058c1ba6f2fe318e6b4c25
João Tinoco <joaoggtinocodev@gmail.com>
2024-11-21 21:16:42 -0300

    Updates

---
+++ static/DtUO3Vz7EATbMR-rLluPr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cb355538-463a9008b5b2de40.js","static/chunks/pages/index-54443fc1fa68c8e7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
